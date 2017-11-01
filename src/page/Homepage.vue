31e8c5083df89b357329e0c3b618d5a2bfd79a82
sun142725 <421818732@qq.com>
2017-11-01 19:24:24 +0800

    手脚架

---
+++ src/page/Homepage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    homepage
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    name: 'Homepage',
+    methods: {},
+    components: {}
+  }
+
+</script>
+<style scoped lang="less" type="text/less"></style>
